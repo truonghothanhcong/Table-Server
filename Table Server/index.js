dd6e7143d300557809edad147b9bde9b8c7fc968
CongTruong <cong@Macbooks-MBP.net.fpt>
2017-07-06 00:26:20 +0700

    add api get list contact, call by phone number, get list music, play music by name

--- Table Server/index.js
+++ Table Server/index.js
@@ -7,26 +7,49 @@ var io = require('socket.io')(http);
 // 	res.sendFile(__dirname + '/index.html');
 // });
 
+
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
 
+	// unity change color mobile
+	app.get('/changeColor', function(req, res){
+		//console.log(req.query.q);
+		socket.emit('changeColor', req.query.q);
+
+		socket.on('colorChanged', function(data){
+			socket.removeAllListeners('colorChanged');
+			return res.send(data);
+
+		})
+	});
+
+
+
 	// unity get list contact
 	app.get('/getListContact', function(req, res){
-		socket.on('getListContact', function(data){
-			res.send(data);
+		socket.emit('getListContact')
+
+		socket.on('listContact', function(data){
+			socket.removeAllListeners('listContact');
+			return res.send(data);
 		})
 	});
 
 	// unity send number to call
-	//app.post('/postNumber', )
+	app.get('/call', function(req, res){
+		socket.emit('call', req.query.number)
+	});
 
 	// unity get list image
-	app.get('/getListImage', function(req, res){
-		socket.on('getListImage', function(data){
-			res.sendFile(data);
+	app.get('/getImages', function(req, res){
+		socket.emit('getImages')
+
+		socket.on('i', function(data){
+			socket.removeAllListeners('images');
+			return res.sendFile(data);
 		})
 	});
 
@@ -35,12 +58,18 @@ io.on('connection', function(socket){
 
 	// unity get list music
 	app.get('/getListMusic', function(req, res){
-		socket.on('getListMusic', function(data){
-			res.send(data);
+		socket.emit('getListMusic')
+
+		socket.on('listMusic', function(data){
+			socket.removeAllListeners('listMusic');
+			return res.send(data);
 		})
 	});
 
-	// unity post name music to play
+	// unity play music with name
+	app.get('/playMusic', function(req, res){
+		socket.emit('playMusic', req.query.name)
+	});
 	
 });
 
